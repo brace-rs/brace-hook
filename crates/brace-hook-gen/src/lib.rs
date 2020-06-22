d1503e2ce34b3ec516cea43f710280120d06d1cb
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-22 12:39:55 +0100

    Move hook generation code to own crate

---
+++ crates/brace-hook-gen/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod args;
+pub mod attr;
+pub mod declaration;
+pub mod lifetime;
+pub mod registration;
+pub mod signature;
--- crates/brace-hook-macros/src/lib.rs
+++ crates/brace-hook-macros/src/lib.rs
@@ -1,21 +1,21 @@
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
+use syn::{parse_macro_input, parse_str};
 
-use self::args::Args;
-
-mod args;
-mod attr;
-mod declaration;
-mod lifetime;
-mod registration;
-mod signature;
+use brace_hook_gen::args::Args;
+use brace_hook_gen::{declaration, registration};
 
 #[proc_macro_attribute]
 pub fn hook(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as Args);
+    let krate = match parse_str("brace_hook") {
+        Ok(path) => path,
+        Err(err) => return TokenStream::from(err.to_compile_error()),
+    };
 
     TokenStream::from(match args {
-        Args::None => declaration::expand(parse_macro_input!(input)),
-        Args::With(path, weight) => registration::expand(path, weight, parse_macro_input!(input)),
+        Args::None => declaration::expand(krate, parse_macro_input!(input)),
+        Args::With(path, weight) => {
+            registration::expand(krate, path, weight, parse_macro_input!(input))
+        }
     })
 }
